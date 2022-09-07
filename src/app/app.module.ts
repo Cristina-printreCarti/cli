37b32bf3b26bf9f936fb058b21d8a6ca64cd92e0
Cristina <cristinamogos.printrecarti@gmail.com>
2022-09-07 10:37:41 +0300

    beginning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListClientsComponent } from './components/list-clients/list-clients.component';
+import { CreateClientComponent } from './components/create-client/create-client.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListClientsComponent,
+    CreateClientComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
